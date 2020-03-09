5ebaaac7b106af846af867b1baac27f03a63902f
santosh0305 <2mymail.santosh@gmail.com>
2020-03-09 08:09:54 +0530

    1 file modified

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css']
 })
 export class ParentComponent implements OnInit {
+  
+  @ViewChild('child') childComponent : ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+    this.childComponent.nativeElement.value = "test";
   }
 
 }
